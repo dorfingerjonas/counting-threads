b3c5800acbee0b00c5adef0900c10b6b81073532
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-02-03 09:12:30 +0100

    add class definition

--- src/main/java/CountingThread.java
+++ src/main/java/CountingThread.java
@@ -1,3 +1,20 @@
-public class CountingThread {
+public class CountingThread extends Thread{
 
+    private Counter counter;
+    private int maxCount;
+    private String threadName;
+
+    public CountingThread(String threadName, Counter counter, int maxCount) {
+        super(threadName);
+        this.counter = counter;
+        this.maxCount = maxCount;
+        this.threadName = threadName;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < maxCount; i++) {
+            counter.increment();
+        }
+    }
 }
