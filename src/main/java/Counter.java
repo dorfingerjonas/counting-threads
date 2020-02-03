3e958bf147254efe4b4a583c7e0bfd6877b9374f
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-02-03 09:07:21 +0100

    add class definition

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,3 +1,16 @@
 public class Counter {
 
+    private int counter;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void increment() {
+        counter++;
+    }
+
+    public void init(int startValue) {
+        counter = startValue;
+    }
 }
