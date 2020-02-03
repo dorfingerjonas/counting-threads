b81479042270de3d847d6fc30aa6a1a31d36fa35
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-02-03 09:30:12 +0100

    add main() with first code, problem occured

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,21 @@
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Counting Threads example");
 
+        long maxCount = 300_000_000;
+
+        Counter counter = new Counter();
+        CountingThread[] threads = new CountingThread[3];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new CountingThread("Counter" + (i + 1), counter, maxCount / threads.length);
+            threads[i].start();
+        }
+
+        for (CountingThread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("Result: " + counter.getCounter());
+    }
 }
