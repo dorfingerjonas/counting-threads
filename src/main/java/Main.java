4275d0b189d135b88ba6ca623fa68959d3416bf4
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-02-10 08:29:51 +0100

    add time calculation for program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ public class Main {
         Counter counter = new Counter();
         CountingThread[] threads = new CountingThread[3];
 
+        long startTime = System.currentTimeMillis();
+
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new CountingThread("Counter" + (i + 1), counter, maxCount / threads.length);
             threads[i].start();
@@ -16,6 +18,11 @@ public class Main {
             thread.join();
         }
 
-        System.out.println("Result: " + counter.getCounter());
+        long stopTime = System.currentTimeMillis();
+        long timeElapsed = stopTime - startTime;
+        System.out.printf("Result: %,d \tDiff: %,d \tTime: %,d ms\n",
+                counter.getCounter(),
+                (maxCount - counter.getCounter()),
+                timeElapsed);
     }
 }
