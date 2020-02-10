a93e1def7d6cf3dd95913507bcbac963f543eb2c
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-02-10 09:07:22 +0100

    implement synchronizedCounter

---
+++ src/main/java/synchronizedCounter.java
@@ -0,0 +1,19 @@
+public class synchronizedCounter implements CounterInterface {
+
+    private long counter = 0;
+
+    @Override
+    public synchronized void increment() {
+        counter++;
+    }
+
+    @Override
+    public long getCounter() {
+        return counter;
+    }
+
+    @Override
+    public void init(long startValue) {
+        counter = startValue;
+    }
+}
